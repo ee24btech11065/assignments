471212800a12bd112091d5e3b6033504bb6e3127
ee24btech11065 <ytnspoorthi2006@gmail.com>
2024-10-26 19:49:57 +0530

    update

--- matgeo1/codes/plot.py
+++ matgeo1/codes/plot.py
@@ -11,70 +11,45 @@ lib.points.restype = ctypes.POINTER(ctypes.c_double)
 result = lib.points()
 
 
-points = [result[i] for i in range(36)]
-A1x, A1y,A1_1x, A1_1y,A11x,A11y,A11_1x,A11_1y,A2x,A2y, A2_1x, A2_1y,A22x,A22y,A22_1x,A22_1y,A3x,A3y,A3_1x,A3_1y,A33x,A33y,A33_1x,A33_1y,A4x,A4y,A4_1x,A4_1y,A44x,A44y,A44_1x,A44_1y,Bx,By,Cx,Cy = points[:36]
+points = [result[i] for i in range(20)]
+Aax, Aay,Aa_1x, Aa_1y,Abx,Aby,Ab_1x,Ab_1y,Acx,Acy, Ac_1x, Ac_1y,Adx,Ady,Ad_1x,Ad_1y,Bx,By,Cx,Cy = points[:20]
 
 
-x_coords = [A1x, A1_1x, A11x, A11_1x,A2x,A2_1x,A22x,A22_1x,A3x,A3_1x,A33x,A33_1x,A4x,A4_1x,A44x,A44_1x,Bx,Cx]
-y_coords = [A1y, A1_1y, A11y,A11_1y,A2y,A2_1y,A22y,A22_1y,A3y,A3_1y,A33y,A33_1y,A4y,A4_1y,A44y,A44_1y,By,Cy]
+x_coords = [Aax, Aa_1x, Abx, Ab_1x,Acx,Ac_1x,Adx,Ad_1x,Bx,Cx]
+y_coords = [Aay, Aa_1y, Aby,Ab_1y,Acy,Ac_1y,Ady,Ad_1y,By,Cy]
 
 
 plt.scatter(x_coords, y_coords)
 
 
 
-custom = ['A1','A1_1','A11','A11_1','A2','A2_1','A22','A22_1','A3','A3_1','A33','A33_1','A4','A4_1','A44','A44_1','B','C']
+custom = ['Aa','Aa_1','Ab','Ab_1','Ac','Ac_1','Ad','Ad_1','B','C']
 
 
 import matplotlib.pyplot as plt
 for i, (x, y) in enumerate(zip(x_coords, y_coords)):
     label = f'{custom[i]} ({x:.2f}, {y:.2f})'  
     plt.annotate(label, xy=(x, y), xytext=(5, 5),
-                 textcoords='offset points', fontsize=8)
+                 textcoords='offset points', fontsize = 8)
+plt.plot([Aax, Bx], [Aay, By], 'k-', lw=2)  
+plt.plot([Aax, Cx], [Aay, Cy], 'k-', lw=2)  
+plt.plot([Aa_1x, Bx], [Aa_1y, By], 'k-', lw=2)  
+plt.plot([Aa_1x, Cx], [Aa_1y, Cy], 'k-', lw=2)  
+plt.plot([Abx, Bx], [Aby, By], 'k-', lw=2)  
+plt.plot([Abx, Cx], [Aby, Cy], 'k-', lw=2)  
+plt.plot([Ab_1x, Bx], [Ab_1y, By], 'k-', lw=2)  
+plt.plot([Ab_1x, Cx], [Ab_1y, Cy], 'k-', lw=2)  
+plt.plot([Acx, Bx], [Acy, By], 'k-', lw=2)  
+plt.plot([Acx, Cx], [Acy, Cy], 'k-', lw=2)  
+plt.plot([Ac_1x, Bx], [Ac_1y, By], 'k-', lw=2)  
+plt.plot([Ac_1x, Cx], [Ac_1y, Cy], 'k-', lw=2)  
+plt.plot([Adx, Bx], [Ady, By], 'k-', lw=2)  
+plt.plot([Adx, Cx], [Ady, Cy], 'k-', lw=2)  
+plt.plot([Ad_1x, Bx], [Ad_1y, By], 'k-', lw=2)  
+plt.plot([Ad_1x, Cx], [Ad_1y, Cy], 'k-', lw=2)  
 
+plt.plot([Cx, Bx], [Cy, By], 'k-', lw=2)  
 
-
-
-
-     #Plot lines between points
-# For example, connecting A1 -> B -> C -> A1_2, etc.
-plt.plot([A1x, Bx], [A1y, By], 'k-', lw=2)  
-plt.plot([Bx, Cx], [By, Cy], 'k-', lw=2)    
-plt.plot([Cx, A1_1x], [Cy, A1_1y], 'k-', lw=2)  
-plt.plot([A1_1x, Bx], [A1_1y, By], 'k-', lw=2) 
-plt.plot([A2x,Cx],[A2y,Cy],'k-',lw=2)
-plt.plot([A1_1x,Cx],[A1_1y,Cy],'k-',lw=2)
-plt.plot([A2_1x,Bx],[A2_1y,By],'k-',lw=2)
-plt.plot([A2_1x,Cx],[A2_1y,Cy],'k-',lw=2)
-plt.plot([A2x, Bx], [A2y, By], 'k-', lw=2)  
-plt.plot([A1x,Cx],[A1y,Cy],'k-',lw=2)
-plt.plot([A11x,Bx],[A11y,By],'k-',lw=2)
-plt.plot([A11x,Cx],[A11y,Cy],'k-',lw=2)
-plt.plot([A11_1x,Cx],[A11_1y,Cy],'k-',lw=2)
-
-plt.plot([A22x, Cx], [A22y, Cy], 'k-', lw=2)
-plt.plot([A22_1x, Cx], [A22_1y, Cy], 'k-', lw=2)  
-plt.plot([A3x, Cx], [A3y, Cy], 'k-', lw=2)  
-plt.plot([A3_1x, Cx], [A3_1y, Cy], 'k-', lw=2)
-plt.plot([A33x, Cx], [A33y, Cy], 'k-', lw=2)  
-plt.plot([A33_1x, Cx], [A33_1y, Cy], 'k-', lw=2)  
-plt.plot([A4x, Cx], [A4y, Cy], 'k-', lw=2)  
-plt.plot([A4_1x, Cx], [A4_1y, Cy], 'k-', lw=2)  
-plt.plot([A44x, Cx], [A44y, Cy], 'k-', lw=2)  
-plt.plot([A44_1x, Cx], [A44_1y, Cy], 'k-', lw=2)  
-
-plt.plot([A11_1x, Bx], [A11_1y, By], 'k-', lw=2)  
-plt.plot([A22x, Bx], [A22y, By], 'k-', lw=2)  
-plt.plot([A22_1x, Bx], [A22_1y, By], 'k-', lw=2)  
-plt.plot([A3x, Bx], [A3y, By], 'k-', lw=2)  
-plt.plot([A3_1x, Bx], [A3_1y, By], 'k-', lw=2)
-plt.plot([A33x, Bx], [A33y, By], 'k-', lw=2)  
-plt.plot([A33_1x, Bx], [A33_1y, By], 'k-', lw=2)  
-plt.plot([A4x, Bx], [A4y, By], 'k-', lw=2)  
-plt.plot([A4_1x, Bx], [A4_1y, By], 'k-', lw=2)  
-plt.plot([A44x, Bx], [A44y, By], 'k-', lw=2)  
-
-plt.plot([A44_1x, Bx], [A44_1y, By], 'k-', lw=2)  
 
 
 plt.xlabel('X')
--- matgeo1/codes/points.c
+++ matgeo1/codes/points.c
@@ -1,79 +1,63 @@
    #include <stdio.h>
 #include <math.h>
 double *points() {
-    
-    static double arr[36];
+    // Points B and C
+    static double arr[20];
     double Bx = 0.0, By = 0.0;
     double Cx = 6.0, Cy = 0.0;
 double sinteeta = sqrt(3)/2;
 double costeeta = 0.5;
-double d1 = 3.2;
-    double AB1 = 7.23;  
-    double AC1 = AB1 - d1;
-    double AC_1 = 7.23;  
-    double AB_1 = AC_1 - d1;
-    double A1x = 6 - AC1 * costeeta;
-    double A1y = AC1 * sinteeta;
-    double A1_1x = A1x;
-    double A1_1y = -A1y;
-    double A11x = 6 - (AC_1)*costeeta;
-    double A11y = (AC_1)*sinteeta;
-    double A11_1x = A11x;
-    double A11_1y = -A11y;
- double d2 = 3.1;
-    double AB2 = 7.36,AC2 = AB2 - d2,AC_2 = 7.36,AB_2 = AC_2 - d2;
-    double A2x = 6 - AC2*costeeta,A2y = AC2*sinteeta,A2_1x = A2x,A2_1y = -A2y;
-    double A22x = 6 - (AC_2)*costeeta,A22y = (AC_2)*sinteeta,A22_1x = A22x,A22_1y = - A22y;double d3 = 3;
-    double AB3 = 7.5,AC3 = AB3 - d3,AC_3 = 7.5,AB_3 = AC_3 - d3;
-    double A3x = 6 - AC3*costeeta,A3y = AC3*sinteeta,A3_1x = A3x,A3_1y = - A3y;
-    double A33x = 6 - (AC_3)*costeeta,A33y = (AC_3)*sinteeta,A33_1x = A33x,A33_1y = -A33y; double d4 = 2.8;
-    double AB4 = 7.82,AC4 = AB4 - d4,AC_4 = 7.82,AB_4 = AC_4 - d4;
-    double A4x = 6 - AC4*costeeta,A4y = AC4*sinteeta,A4_1x = A4x,A4_1y = -A4y;
-    double A44x = 6- (AC_4)*costeeta,A44y = (AC_4)*sinteeta,A44_1x = A44x,A44_1y = - A44y;
- arr[0] = A1x;
-     arr[1]=A1y;
-     arr[2] = A1_1x;
-     arr[3] = A1_1y;
-     arr[4] = A11x;
-     arr[5] = A11y;
-     arr[6] = A11_1x;
-     arr[7]=A11_1y;
-     arr[8]=A2x;
-     arr[9]=A2y;
-     arr[10]=A2_1x;
-     arr[11]=A2_1y;
-     arr[12]=A22x; 
-     arr[13]= A22y;
-     arr[14]= A22_1x;
-     arr[15] = A22_1y;
-     arr[16]= A3x;
-     arr[17]=A3y;
-     arr[18]=A3_1x;
-     arr[19]=A3_1y;
-     arr[20]=A33x;
-     arr[21]=A33y;
-     arr[22]=A33_1x;
-     arr[23]=A33_1y;
-     arr[24]= A4x;
-     arr[25]=A4y;
-     arr[26]=A4_1x;
-     arr[27]=A4_1y;
-     arr[28]=A44x;
-     arr[29]=A44y;
-     arr[30]=A44_1x;
-     arr[31]=A44_1y;
-     arr[32]=Bx;
-     arr[33]=By;
-     arr[34]=Cx;
-     arr[35]=Cy;
+double d1 = 3.2,d2 = 3.1,d3=3,d4=2.8;
+
+double AC1 = (36 - d1*d1)/(6 + 2*d1);
+    
+    double Aax = 6 - AC1 * costeeta,Aay = AC1 * sinteeta;
+    double Aa_1x = Aax, Aa_1y = -Aay;
+
+double AC2 = (36 - d2*d2)/(6 + 2*d2);
+    double Abx = 6 - (AC2)*costeeta,Aby = (AC2)*sinteeta;
+    double Ab_1x = Abx,Ab_1y = -Aby;
+ 
+double AC3 = (36 - d3*d3)/(6 + 2*d3);
+    
+    double Acx = 6 - AC3*costeeta,Acy = AC3*sinteeta;
+    double Ac_1x = Acx,Ac_1y = -Acy;
+    
+
+double AC4 = (36 - d4*d4)/(6 + 2*d4);
+
+    double Adx = 6 - AC4*costeeta,Ady = AC4*sinteeta;
+    double Ad_1x = Adx,Ad_1y = - Ady;
+    
+    
+ arr[0] = Aax;
+     arr[1]=Aay;
+     arr[2] = Aa_1x;
+     arr[3] = Aa_1y;
+     arr[4] = Abx;
+     arr[5] = Aby;
+     arr[6] = Ab_1x;
+     arr[7]=Ab_1y;
+     arr[8]=Acx;
+     arr[9]=Acy;
+     arr[10]=Ac_1x;
+     arr[11]=Ac_1y;
+     arr[12]=Adx; 
+     arr[13]= Ady;
+     arr[14]= Ad_1x;
+     arr[15] = Ad_1y;
+     arr[16]=Bx;
+     arr[17]=By;
+     arr[18]=Cx;
+     arr[19]=Cy;
     return arr;
 }
 int main() {
     double* coordinates = points();
 
-    
+    // Print the coordinates of the points
     printf("Coordinates of points:\n");
-    for (int i = 0; i <= 34; i += 2) {
+    for (int i = 0; i <= 18; i += 2) {
         printf("Point %d: (%.2f, %.2f)\n", i / 2 + 1, coordinates[i], coordinates[i + 1]);
     }
 
